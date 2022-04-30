ae246f046e8df2ff0569d45a76784bc71beb29c9
yonatankra <yonatan.kra@vonage.com>
2022-04-30 10:02:52 +0300

    Convert __dirname

--- libs/components/src/lib/text/ui.test.ts
+++ libs/components/src/lib/text/ui.test.ts
@@ -11,7 +11,7 @@ const components = ['text'];
 
 test('should show the component', async ({page}: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
-		path.join(__dirname, 'README.md')
+		path.join(new URL('.', import.meta.url).pathname, 'README.md')
 	)
 		.reduce(
 			(htmlString: string, block: string) =>
